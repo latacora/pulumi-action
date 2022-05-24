c024bfb3151ca919bf115c1c183a18766a4a00bc
Avi Flax @ Latacora <avif@latacora.com>
2022-05-23 20:34:11 -0400

    WIP++

--- src/main.ts
+++ src/main.ts
@@ -92,7 +92,7 @@ const main = async () => {
   }
 
   if (config.commentOnPr) {
-    core.debug(`Commenting on pull request`);
+    core.info(`Commenting on pull request`);
     invariant(config.githubToken, 'github-token is missing.');
     handlePullRequestMessage(config, output);
   }
